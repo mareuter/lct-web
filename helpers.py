33681deeb53f211c32cfdd60260918dbee1a2593
Michael Reuter <mareuternh@gmail.com>
2017-01-05 19:25:43 -0700

    Finishing out moon info JSON.

--- helpers.py
+++ helpers.py
@@ -1,12 +1,13 @@
 from datetime import datetime
+import math
 
 from pylunar import MoonInfo
 
 def convert_dec_loc_to_loc_tuple(loc):
     degrees = int(loc)
-    minutes = int((loc - degrees) * 60.0)
-    seconds = int((loc - degrees) * 3600.0)
-    return (degrees, minutes, seconds)
+    minutes = math.fabs((loc - degrees)) * 60.0
+    seconds = (minutes - round(minutes)) * 60.0
+    return (degrees, round(minutes), round(seconds))
 
 def get_moon_info(date, lat, lon):
     d = datetime.fromtimestamp(date)
@@ -16,5 +17,9 @@ def get_moon_info(date, lat, lon):
 
     moon_info = MoonInfo(lat_tuple, lon_tuple)
     moon_info.update(date_tuple)
+    print(moon_info.observer)
 
-    return {"age": moon_info.age()}
+    return {"age": moon_info.age(), "colong": moon_info.colong(),
+            "fractional_phase": moon_info.fractional_phase(), "libration_lon": moon_info.libration_lon(),
+            "libration_lat": moon_info.libration_lat(), "altitude": moon_info.altitude(),
+            "azimuth": moon_info.azimuth(), "next_four_phases": moon_info.next_four_phases()}
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,9 +15,19 @@ class TestApp(object):
 
     def test_moon_info(self):
         with app.test_request_context():
-            moon_info_url = url_for("moon_info", date=1382158800.0, lat=-84.316666666666,
-                                    lon=35.9694444444444)
+            moon_info_url = url_for("moon_info", date=1382158800.0, lat=35.9694444444444,
+                                    lon=-84.316666666666)
             response = self.client.get(moon_info_url)
             moon_info = json.loads(response.data)
             assert response.status_code == 200
             assert moon_info["age"] == 13.892695861570246
+            assert moon_info["colong"] == 83.97189956624061
+            assert moon_info["fractional_phase"] == 0.9998519924481626
+            assert moon_info["libration_lon"] == 5.23107551788429
+            assert moon_info["libration_lat"] == -1.4788210646482465
+            assert moon_info["altitude"] == -9.8149186580585
+            assert moon_info["azimuth"] == 69.75156520051686
+            next_four_phases = moon_info["next_four_phases"]
+            assert len(next_four_phases) == 4
+            assert next_four_phases[0][0] == "full"
+            assert next_four_phases[0][1] == [2013, 10, 18, 23, 37, 39.644067962653935]
