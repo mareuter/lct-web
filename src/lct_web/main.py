f3326394e9971799e0c7bf32eeabe5f6c4ae0e25
Michael Reuter <mareuternh@gmail.com>
2024-03-10 20:46:47 -0700

    Update openapi page.

--- src/lct_web/main.py
+++ src/lct_web/main.py
@@ -12,8 +12,12 @@
 
 from __future__ import annotations
 
-from fastapi import FastAPI
+from typing import Any
 
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+from . import __version__
 from .models.index_response import IndexResponse
 from .routers import lunar_club, lunar_two, moon_info
 
@@ -24,6 +28,29 @@ app = FastAPI()
 app.include_router(moon_info.router)
 app.include_router(lunar_club.router)
 app.include_router(lunar_two.router)
+
+
+def set_schema() -> dict[str, Any]:
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Lunar Club Tools Web Service API",
+        version=__version__,
+        description=" ".join(
+            [
+                "This API provides information related to the Lunar Club and Lunar II ",
+                "observing clubs from the Astronomical League. It also provides ephemeris",
+                "information.",
+            ]
+        ),
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = set_schema  # type: ignore
 
 
 @app.get("/")
